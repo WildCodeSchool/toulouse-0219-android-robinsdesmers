478ca54fc60843199ccd989841ce9f1ead78cb3d
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-05-22 12:01:14 +0200

    design de la page Inscripiton

---
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/Inscription.java
@@ -0,0 +1,26 @@
+package fr.wildcodeschool.robinsdesmers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Inscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+
+        ImageButton imBtRegister = findViewById(R.id.imBtRegister);
+
+        imBtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToHome = new Intent(Inscription.this, MainActivity.class);
+                startActivity(goToHome);
+            }
+        });
+    }
+}
