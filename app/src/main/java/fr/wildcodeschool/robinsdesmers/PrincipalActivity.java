381bff8c40f147c21d82e3be9427a53c14a87c6e
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-05-23 11:30:13 +0200

    Création de la page principale

---
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/PrincipalActivity.java
@@ -0,0 +1,46 @@
+package fr.wildcodeschool.robinsdesmers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        ImageButton imBtRegister = findViewById(R.id.ibRegisterPrincipal);
+
+        imBtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRegister = new Intent(PrincipalActivity.this, InscriptionActivity.class);
+                startActivity(goToRegister);
+            }
+        });
+
+        ImageButton imBtSignIn = findViewById(R.id.ibSignPrincipal);
+
+        imBtSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToSignIn = new Intent(PrincipalActivity.this, MainActivity.class);
+                startActivity(goToSignIn);
+            }
+        });
+
+        ImageButton imBtVisitor = findViewById(R.id.ibVisitorPrincipal);
+
+        imBtVisitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToHome = new Intent(PrincipalActivity.this, MainActivity.class);
+                startActivity(goToHome);
+            }
+        });
+    }
+}
