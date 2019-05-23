22765deb175b83a36fd3da4c2957bdcfc15b9b24
Sébastien <snx.dubois@gmail.com>
2019-05-23 11:15:04 +0200

    nettoyage du code avant envoi de la branche

--- app/src/main/java/fr/wildcodeschool/robinsdesmers/UserProfile.java
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/UserProfile.java
@@ -2,9 +2,9 @@ package fr.wildcodeschool.robinsdesmers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.NonNull;
 import android.view.MenuItem;
 
 public class UserProfile extends AppCompatActivity {
