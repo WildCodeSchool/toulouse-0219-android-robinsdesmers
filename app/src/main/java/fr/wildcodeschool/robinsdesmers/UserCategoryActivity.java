8770328b97b44992e250224551284181ba45e9a8
Lilia Jerbi <lilia-jerbi@hotmail.fr>
2019-05-29 17:29:23 +0200

    design

---
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/UserCategoryActivity.java
@@ -0,0 +1,20 @@
+package fr.wildcodeschool.robinsdesmers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static fr.wildcodeschool.robinsdesmers.R.drawable.button_border;
+
+public class UserCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_category);
+
+    }
+
+
+}
