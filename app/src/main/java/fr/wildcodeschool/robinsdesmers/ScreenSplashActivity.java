1ea97150c1367422c5e681aa6e3ea920b16cb224
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-06-05 17:44:14 +0200

    Clean

--- app/src/main/java/fr/wildcodeschool/robinsdesmers/ScreenSplashActivity.java
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/ScreenSplashActivity.java
@@ -1,12 +1,16 @@
 package fr.wildcodeschool.robinsdesmers;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 public class ScreenSplashActivity extends AppCompatActivity {
+    final int MILLIS = 3000;
+    final int ROTATION_START = 0;
+    final int ROTATION_END = 600;
+    final int DURATION = 800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,15 +18,15 @@ public class ScreenSplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_screen_splash);
 
         ImageView imageView = findViewById(R.id.ivScreenImg);
-        RotateAnimation rotate = new RotateAnimation(0 ,600);
-        rotate.setDuration(800);
+        RotateAnimation rotate = new RotateAnimation(ROTATION_START, ROTATION_END);
+        rotate.setDuration(DURATION);
         imageView.startAnimation(rotate);
 
         Thread myThread = new Thread() {
 
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(MILLIS);
                     Intent intent = new Intent(ScreenSplashActivity.this, FirstPageActivity.class);
                     startActivity(intent);
                     finish();
