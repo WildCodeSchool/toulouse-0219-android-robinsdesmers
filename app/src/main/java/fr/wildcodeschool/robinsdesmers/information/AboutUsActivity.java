75f01b88ec3ba1f94c3acb13a75e1ee9b6ae88f0
Sébastien <snx.dubois@gmail.com>
2019-06-13 12:06:45 +0200

    recup branche dev

---
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/information/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package fr.wildcodeschool.robinsdesmers.information;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fr.wildcodeschool.robinsdesmers.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
