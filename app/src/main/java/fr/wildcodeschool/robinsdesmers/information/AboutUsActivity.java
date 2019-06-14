e0b990928e61766307eb5f6a1536862a1df61f1e
Sébastien <snx.dubois@gmail.com>
2019-06-14 13:37:59 +0200

    Invite friends button on about us activity added.

--- app/src/main/java/fr/wildcodeschool/robinsdesmers/information/AboutUsActivity.java
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/information/AboutUsActivity.java
@@ -2,19 +2,32 @@ package fr.wildcodeschool.robinsdesmers.information;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.appinvite.AppInvite;
+import com.google.android.gms.appinvite.AppInviteInvitation;
+import com.google.android.gms.appinvite.AppInviteInvitationResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 
 import fr.wildcodeschool.robinsdesmers.MainActivity;
 import fr.wildcodeschool.robinsdesmers.MapsActivity;
 import fr.wildcodeschool.robinsdesmers.R;
 import fr.wildcodeschool.robinsdesmers.UserProfileActivity;
 
-public class AboutUsActivity extends AppCompatActivity {
+public class AboutUsActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_INVITE = 101;
+    private GoogleApiClient mGoogleApiClient;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -52,6 +65,64 @@ public class AboutUsActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        findViewById(R.id.btInviteFriends).setOnClickListener(this);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(AppInvite.API)
+                .enableAutoManage(this, this)
+                .build();
+
+        boolean autoLaunchDeepLink = true;
+        AppInvite.AppInviteApi.getInvitation(mGoogleApiClient, this, autoLaunchDeepLink)
+                .setResultCallback(
+                        new ResultCallback<AppInviteInvitationResult>() {
+                            @Override
+                            public void onResult(AppInviteInvitationResult result) {
+                                Log.d(TAG, "getInvitation:onResult:" + result.getStatus());
+                            }
+                        });
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        showMessage("Google Play services error");
+    }
+
+    private void onInviteClicked() {
+
+        /*Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(Intent.EXTRA_TEXT,"https://play.google.com/store/apps/details?id=fr.wildcodeschool.metro");
+        startActivity(Intent.createChooser(sharingIntent, "How do you want to share?"));*/
+
+        Intent intent = new AppInviteInvitation.IntentBuilder("Invite tes amis à rejoindre la communauté")
+                .setMessage("Salut! J'ai découvert cette super communauté et je t'invite à la rejoindre")
+                .setDeepLink(Uri.parse("https://play.google.com/store/apps/details?id=fr.wildcodeschool.metro"))
+                .setCustomImage(Uri.parse("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"))
+                .setCallToActionText("Installer")
+                .build();
+        startActivityForResult(intent, REQUEST_INVITE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: requestCode=" + requestCode + ", resultCode=" + resultCode);
+
+        if (requestCode == REQUEST_INVITE) {
+            if (resultCode == RESULT_OK) {
+                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
+                Log.d(TAG, ids + " invitations envoyées");
+            } else {
+                showMessage("L'envoi des invitations a échoué");
+            }
+        }
+    }
+
+    private void showMessage(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 
     public void openRdm(View view) {
@@ -72,5 +143,14 @@ public class AboutUsActivity extends AppCompatActivity {
     public void openTwitter(View view) {
         Intent twitterWebsite = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/RobinsDesmers"));
         startActivity(twitterWebsite);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btInviteFriends:
+                onInviteClicked();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/information/DeepLinkActivity.java
@@ -0,0 +1,56 @@
+package fr.wildcodeschool.robinsdesmers.information;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.appinvite.AppInviteReferral;
+
+import fr.wildcodeschool.robinsdesmers.R;
+
+public class DeepLinkActivity extends AppCompatActivity implements
+        View.OnClickListener {
+
+    private static final String TAG = DeepLinkActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_link);
+
+        findViewById(R.id.button_ok).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent intent = getIntent();
+        if (AppInviteReferral.hasReferral(intent)) {
+            processReferralIntent(intent);
+        }
+    }
+
+    private void processReferralIntent(Intent intent) {
+        String invitationId = AppInviteReferral.getInvitationId(intent);
+        String deepLink = AppInviteReferral.getDeepLink(intent);
+
+        Log.d(TAG, "Found Referral: " + invitationId + ":" + deepLink);
+        ((TextView) findViewById(R.id.deep_link_text))
+                .setText("Deep Link : " + deepLink);
+        ((TextView) findViewById(R.id.invitation_id_text))
+                .setText("Invitation: " + invitationId);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_ok:
+                finish();
+                break;
+        }
+    }
+}
