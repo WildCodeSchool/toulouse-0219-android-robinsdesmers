be9542a7475479b32760367947755cd1620cf412
Sébastien <snx.dubois@gmail.com>
2019-07-04 09:46:32 +0200

    cleanup collectpoint info activity, removed unnecessary volley calls, deleted obsolete updateUserScore method

--- app/src/main/java/fr/wildcodeschool/robinsdesmers/VolleySingleton.java
+++ app/src/main/java/fr/wildcodeschool/robinsdesmers/VolleySingleton.java
@@ -19,7 +19,6 @@ import com.google.gson.GsonBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -290,30 +289,6 @@ public class VolleySingleton {
                     public void onResponse(JSONObject response) {
                         RubbishItem rubbishItem = gson.fromJson(response.toString(), RubbishItem.class);
                         rubbishListener.accept(rubbishItem);
-                    }
-                }, new Response.ErrorListener() {
-
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.e("Error: ", error.getMessage());
-            }
-        });
-        requestQueue.add(jsonObjectRequest);
-    }
-
-    public void updateUserScore(Long userId, final Consumer<User> userListener) {
-        String url = REQUEST_URL + "users/" + userId + "/score";
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        final Gson gson = gsonBuilder.create();
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.PUT, url, null,
-                new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        User user = gson.fromJson(response.toString(), User.class);
-                        userListener.accept(user);
                     }
                 }, new Response.ErrorListener() {
 
